eddb7afe0a5cd81ad02009922564ca7ea24cdc45
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2021-01-15 16:26:06 +0600

    initial department methods insert, edit and remove

--- imports/api/departments.js
+++ imports/api/departments.js
@@ -1,3 +1,22 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Departments = new Mongo.Collection('departments');
+
+// TODO: validation
+// TODO: duplicate department
+Meteor.methods({
+  'departments.insert'(department = {}) {
+    return Departments.insert(department);
+  },
+  'departments.update'(_id = '', department = {}) {
+    return Departments.update(
+      {
+        _id,
+      },
+      { $set: department }
+    );
+  },
+  'departments.remove'(_id = '') {
+    return Departments.remove(_id);
+  },
+});
